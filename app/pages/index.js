0bd95e1d4c61f393262c1623a0c016a179cf72b3
Matija Gaspar <matija.gaspar@gmail.com>
2018-10-26 01:29:07 +0200

    add attribution to pdfs, fix readme,...

--- app/pages/index.js
+++ app/pages/index.js
@@ -170,9 +170,6 @@ class Index extends React.Component {
                     </Otherwise>
                 </Choose>
 
-                <Footer>
-                    <div>Equipment icons made by <a href='https://www.flaticon.com/authors/smashicons' title='Smashicons'>Smashicons</a>, <a href='https://www.flaticon.com/authors/pixel-buddha' title='Pixel Buddha'>Pixel Buddha</a> and  <a href='http://www.freepik.com/' title='Freepik'>Freepik</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a> is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0' target='_blank'>CC 3.0 BY</a>, everything else by team Blue Ice</div>
-                </Footer>
                 {/* </RequireAuth> */}
             </App>
         )
